71e8a4dc114954c0ba49d759afcb3eaf4a3493be
William <witrandev@gmail.com>
2020-05-27 21:39:46 +0700

    Fix import

--- maggie/__init__.py
+++ maggie/__init__.py
@@ -1,2 +1,4 @@
 from .node import Node
 from .operators import *
+from .modules import *
+from .functions import *
---
+++ maggie/modules.py
@@ -0,0 +1,9 @@
+class Module:
+    def __init__(self):
+        pass
+
+    def forward():
+        raise NotImplementedError
+
+    def __call__(self, *args, **kwargs):
+        return forward(*args, **kwargs)
